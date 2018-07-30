03134be09c0577ecbcd06b4ece9d6c09fc0e06a6
liuchao111 <18511288299@163.com>
2018-07-30 16:34:24 +0800

    增加退出功能

--- pyspider1.02.py
+++ pyspider1.02.py
@@ -5,21 +5,17 @@ import json
 
 
 if __name__ == "__main__":
-    Request_URL = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
-    form_data = {}
-    form_data['to'] = 'AUTO'
-    form_data['from'] = 'AUTO'
-    form_data['i'] = input('请输入要查询的英文: ')
-    form_data['doctype'] = 'json'
-    form_data['version'] = '2.1'
-    form_data['smartresult'] = 'dict'
-    form_data['keyfrom'] = 'fanyi.web'
-    form_data['client'] = 'fanyideskweb'
-    form_data['action'] = 'FY_BY_REALTIME'
-    form_data['typoResult'] = 'false'
-    data = parse.urlencode(form_data).encode('utf-8')
-    response = request.urlopen(Request_URL, data)
-    html = response.read().decode('utf-8')
-    translate_results = json.loads(html)
-    translate_results = translate_results['translateResult'][0][0]['tgt']
-    print("翻译结果就是\n%s" % translate_results)
+    while True:
+        content = input('请输入要查询的英文按(1)退出: ')
+        if content == '1':
+            break
+        Request_URL = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
+        form_data = {'to': 'AUTO', 'from': 'AUTO', 'i': content, 'doctype': 'json', 'version': '2.1',
+                     'smartresult': 'dict', 'keyfrom': 'fanyi.web', 'client': 'fanyideskweb', 'action': 'FY_BY_REALTIME',
+                     'typoResult': 'false'}
+        data = parse.urlencode(form_data).encode('utf-8')
+        response = request.urlopen(Request_URL, data)
+        html = response.read().decode('utf-8')
+        translate_results = json.loads(html)
+        translate_results = translate_results['translateResult'][0][0]['tgt']
+        print("翻译结果就是\n%s" % translate_results)
